1fdaeb642f9edd44128b07a0c5f07f621786a881
Jamie Briggs <personal@jbriggs.dev>
2025-10-26 19:22:17 +0000

    First commit

---
+++ src/main/java/dev/jbriggs/flood/api/FloodApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jbriggs.flood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodApiApplication.class, args);
+	}
+
+}
